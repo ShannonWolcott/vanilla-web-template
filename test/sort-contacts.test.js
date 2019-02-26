6da8488ff1610cef150cea6e561088a07309cf11
martypdx <marty@martypdx.com>
2019-02-26 14:58:38 -0800

    working sort function

--- test/sort-contacts.test.js
+++ test/sort-contacts.test.js
@@ -1,38 +1,16 @@
 const test = QUnit.test;
 
-function sort(contacts, sortField) {
+function sort(contacts, sortField, direction) {
+    direction = direction || 1;
+
     return contacts.sort((a, b) => {
         if(a[sortField] === b[sortField]) {
             return 0;
         }
         if(a[sortField] > b[sortField]) {
-            return 1;
+            return 1 * direction;
         }
-        return -1;
-    });
-}
-
-function sortFirstName(contacts) {
-    return contacts.sort((a, b) => {
-        if(a.firstName === b.firstName) {
-            return 0;
-        }
-        if(a.firstName > b.firstName) {
-            return 1;
-        }
-        return -1;
-    });
-}
-
-function sortAge(contacts) {
-    return contacts.sort((a, b) => {
-        if(a.age === b.age) {
-            return 0;
-        }
-        if(a.age > b.age) {
-            return 1;
-        }
-        return -1;
+        return -1 * direction;
     });
 }
 
@@ -62,6 +40,18 @@ test('sort by age', assert => {
     assert.deepEqual(sorted, [
         { firstName: 'Bert', age: 12 },
         { firstName: 'Zena', age: 14 },
+        { firstName: 'Albert', age: 16 }
+    ]);
+});
+
+test('sort by firstName descending', assert => {
+    //arrange (contacts)
+    //act
+    const sorted = sort(contacts, 'firstName', -1);
+    //assert
+    assert.deepEqual(sorted, [
+        { firstName: 'Zena', age: 14 },
+        { firstName: 'Bert', age: 12 },
         { firstName: 'Albert', age: 16 }
     ]);
 });
